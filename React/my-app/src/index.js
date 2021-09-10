b7b68e6f5ef437a41a361a9b136464428cdcb74b
Jon Andoni Galdos <jonandonigv@gmail.com>
2021-09-11 00:40:42 +0200

    update square state

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <button className='square'>
-                {/* TODO */}
+            <button className='square' onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
@@ -14,7 +21,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />;
+        return <Square value={i}/>;
     }
 
     render() {
@@ -29,14 +36,14 @@ class Board extends React.Component {
                     {this.renderSquare(2)}
                 </div>
                 <div className='board-row'>
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
                 </div>
                 <div className='board-row'>
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
                 </div>
             </div>
         );
