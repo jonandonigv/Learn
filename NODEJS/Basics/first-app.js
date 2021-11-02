2fa34eb57947bfcdd33e3c5e6de62b73441662f3
Jon Andoni <jonandonigv@gmail.com>
2021-11-03 00:26:17 +0100

    first basic app

---
+++ NODEJS/Basics/first-app.js
@@ -0,0 +1,6 @@
+// Imports fs
+const fs = require('fs');
+// creates a file calling the method from fs 
+fs.writeFileSync('hello.txt', 'Hello from nodejs');
+// Outputs the msg to the terminal  
+console.log("Hello world");
